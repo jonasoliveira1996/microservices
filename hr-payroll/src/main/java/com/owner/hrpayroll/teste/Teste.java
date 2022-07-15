3332793cac1b441b91a02453642998678829f241
Jonas Menotti <jonasoliveira1996@gmail.com>
2022-07-15 14:32:59 -0300

    Configuring api gateway

--- hr-payroll/src/main/java/com/owner/hrpayroll/teste/Teste.java
+++
@@ -1,9 +0,0 @@
-package com.owner.hrpayroll.teste;
-
-public class Teste {
-
-    public static void main(String[] args) {
-        Carro carro = new Carro("Toyota", "azul");
-        carro.getCor();
-    }
-}
