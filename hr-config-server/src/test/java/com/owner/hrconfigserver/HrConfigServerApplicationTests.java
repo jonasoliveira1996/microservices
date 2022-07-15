76c981f52cb88d0a7e229c3fc27c54d721aa19b8
Jonas Menotti <jonasoliveira1996@gmail.com>
2022-07-15 15:21:22 -0300

    hr-config-server created

---
+++ hr-config-server/src/test/java/com/owner/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owner.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
