0155aa421537ba0031bf39f9e666872d320ccfc4
Jonas Menotti <jonasoliveira1996@gmail.com>
2022-07-25 12:24:50 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/owner/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owner.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
